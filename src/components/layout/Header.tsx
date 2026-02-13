88aedf6a682d4b20c4017fd013bab854178329b5
arron-kukadia <aksnaps1397@gmail.com>
2026-02-13 13:41:55 +0000

    fix vulnerability

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,6 +14,7 @@ import { usePersonalInfo } from '@/hooks/usePersonalInfo'
 const navItems = [
   { href: '/', label: 'Home' },
   { href: '/projects', label: 'Projects' },
+  { href: '/blog', label: 'Blog' },
   { href: '/about', label: 'About & Experience' },
 ]
 
